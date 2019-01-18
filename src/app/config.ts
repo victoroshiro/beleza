887e82328348daece1865607c84d5f231a9d1dd5
mdd300 <victoroshiro_mdd300@hotmail.com>
2019-01-18 15:05:52 -0200

    ajustes

---
+++ src/app/config.ts
@@ -0,0 +1,12 @@
+//export const WEB = {
+  //  api: "http://aplicativo.bele/api",
+   // url: "http://192.168.0.102/beleza.com-master/web/web/",
+    //socket: "ws://192.168.0.102",
+   // googleApiKey: "AIzaSyA31qhfrQoLVgQe7DVC3lVrofvQIB8x554"
+//}
+ export const WEB = {
+     api: "https://plataforma.aplicativobeleza.com/api",
+     url: "https://plataforma.aplicativobeleza.com",
+     socket: "ws://149.56.74.5:9998",
+     googleApiKey: "AIzaSyA31qhfrQoLVgQe7DVC3lVrofvQIB8x554"
+ }
